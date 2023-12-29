6987ac7c22a0cd0305a107b0e9db8da434c935a0
javipereyra <javypereyra8@gmail.com>
2023-12-28 22:54:39 -0300

    segundapreentrega

--- main.js
+++ main.js
@@ -6,21 +6,39 @@
 
 
 
-function alumno(nombre, apellido, edad){
-    console.log(nombre," ", apellido," " ,edad);
+function alumno(listaDealumnos){       // imprime lista de alumnos 
+    console.log(listaDealumnos); 
+}
+
+function apellidoAusente(listaDealumnos, ausente){      // verifica si alumno esta ausente
+    if(listaDealumnos.includes(ausente)===true) {
+        console.log("El alumno esta presente" );} 
+
+    else {
+        console.log("El alumno esta ausente" );}
 
 }
 
-let cantidadAlumnos=2;
+const listaDealumnos=[];
+
+let cantidadAlumnos=prompt("ingrese cantidad de alumnos");
+
 let sumaEdad=0;
 
 for(let i=0; i<cantidadAlumnos; i++){
-    let nombreAlumno= prompt("ingresar nombre");
     let apellidoAlumno= prompt("ingresar apellido");
     let edadAlumno= Number(prompt("ingresar edad"));
-    alumno(nombreAlumno,apellidoAlumno,parseInt(edadAlumno))
+    listaDealumnos.push(apellidoAlumno); //cargo alumnos en arreglo vacio
+
     sumaEdad=sumaEdad+edadAlumno;
 
 }
+let ausente=prompt("Ingrese a apellido alumno para saber si esta ausente")
+apellidoAusente(listaDealumnos,ausente);// llamo a funcion para verificar ausencia
+
+
+alumno(listaDealumnos)//muestro listo de alumnos
+
 let promedioEdad= sumaEdad/cantidadAlumnos;
-parseFloat(console.log("El promedio de edad de alumnos: " , promedioEdad));
+parseFloat(console.log("El promedio de edad de alumnos: " , promedioEdad));// muestro promedio
+
